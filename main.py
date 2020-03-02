3f3b2c0ec396248c5b1369e071f896bd4c2469b5
gamcoh <cohengamliel8@gmail.com>
2020-03-02 10:24:07 +0100

    added comments

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
-import slack
+import os
 import random
 
-import os
+import slack
 from dotenv import load_dotenv
 
 def get_students(filepath: str = './students.csv') -> list:
+	"""Get the list of the students, returns a list
+	
+	Keyword Arguments:
+		filepath {str} -- the path of the csv file (default: {'./students.csv'})
+	
+	Returns:
+		list -- list of students
+	"""	
 	with open(filepath) as file:
 		return file.readlines()
 
 def generate_groups(nstudents: int = 2) -> dict: # change the number of nstudents if we don't want binoms anymore
+	"""Generate the groups
+
+	Keyword Arguments:
+		nstudents {int} -- the number of students we want per group (default: {2})
+	
+	Returns:
+		dict -- dictionary with key => group, value => list of student for this group
+	"""	
 	groups = {}
 	students = get_students()
 	num_groups = len(students) // nstudents
@@ -26,6 +42,11 @@ def generate_groups(nstudents: int = 2) -> dict: # change the number of nstudent
 	return groups
 
 def generate_message() -> str:
+	"""Generate the message to send
+	
+	Returns:
+		str -- returns the message
+	"""	
 	msg = ''
 	for group, students in generate_groups().items():
 		students = '\n'.join(students)
@@ -43,4 +64,3 @@ def main():
 
 if __name__ == '__main__':
 	main()
-
